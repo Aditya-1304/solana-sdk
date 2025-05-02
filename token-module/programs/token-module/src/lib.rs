2149cde7092dfa5d00f5cd939dae58883a6fa0bd
Aditya Mandal <mandaladitya1614@gmail.com>
2025-05-02 14:09:35 +0530

    struct account declaration for solana txn and spl-acc creation

--- token-module/programs/token-module/src/lib.rs
+++ token-module/programs/token-module/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::TokenInterface;
+use anchor_spl::{token::Mint, token_interface::TokenInterface};
 
 declare_id!("DDnDEV5j1HkJzzV94sLaEi11e2CjXfTFRpQv1amgLxTr");
 
@@ -12,40 +12,76 @@ pub mod token_module {
         Ok(())
     }
 }
+#[derive(Accounts)]
+pub struct InitializeTokenAuthority<'info> {
+    #[account(mut)]
+    pub admin: Signer<'info>,
+
+    #[account(
+        init,
+        payer = admin,
+        space = 8 + 32 + 1,
+        seeds = [b"token_authority"],
+        bump,
+    )]
+    pub token_authority: Account<'info, TokenAuthority>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(name: String, symbol: String, decimals: u8)]
+pub struct CreateToken<'info> {
+    #[account(mut)]
+    pub admin: Signer<'info>,
+
+    #[account(
+        init,
+        payer = admin,
+        mint::decimals = decimals,
+        mint::authority = token_authority,
+        mint::freeze_authority = token_authority,
+    )]
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        init,
+        payer = admin,
+        space = 8 + 32 + 4 + name.len() + 4 + symbol.len() + 1 + 1 + 8 + 32 + 32 + 1,
+        seeds = [b"token_metadata", mint.key().as_ref()],
+        bump
+    )]
+    pub token_metadata: Account<'info, TokenMetadata>,
+
+    ///CHECK: This is the token autority PDA
+    #[account(
+        seeds = [b"token_authority"],
+        bump
+    )]
+    pub token_authority: Account<'info, TokenAuthority>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info,Rent>,
+
+}
 
 #[derive(Accounts)]
 pub struct TransferSol<'info> {
     #[account(mut)]
     pub from: Signer<'info>,
-    ///Check: Receiver account can be any account, not necessarily initialized
+
     #[account(mut)]
+    ///CHECK: Receiver account - system program handles validation 
     pub to: AccountInfo<'info>,
 
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct MintSplToken<'info> {
-    #[account(mut)]
-    pub mint: InterfaceAccount<'info, Mint>,
-    #[account(mut)]
-    pub recipent_token_account: InterfaceAccount<'info, TokenAccount>,
-
-    pub sender_authority: Signer<'info>,
-    pub token_program: Interface<'info, TokenInterface>
-}
 
 
-#[derive(Accounts)]
-pub struct BurnSplToken<'info> {
-    #[account(mut)]
-    pub mint: InterfaceAccount<'info, Mint>,
-    #[account(mut)]
-    pub sender_token_account: InterfaceAccount<'info, TokenAccount>,
 
-    pub sender_authority: Signer<'info>,
-    pub token_program: Interface<'info, TokenInterface>
-}
 
 
 #[account]
