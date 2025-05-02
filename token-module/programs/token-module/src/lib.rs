0f0f8ad8950d78e6ccf336b2594185c9216f34c4
Aditya Mandal <mandaladitya1614@gmail.com>
2025-05-02 12:53:24 +0530

    Basic account declaration for token transfer

--- token-module/programs/token-module/src/lib.rs
+++ token-module/programs/token-module/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token_interface::TokenInterface;
 
 declare_id!("DDnDEV5j1HkJzzV94sLaEi11e2CjXfTFRpQv1amgLxTr");
 
@@ -13,4 +14,66 @@ pub mod token_module {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct TransferSol<'info> {
+    #[account(mut)]
+    pub from: Signer<'info>,
+    ///Check: Receiver account can be any account, not necessarily initialized
+    #[account(mut)]
+    pub to: AccountInfo<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct MintSplToken<'info> {
+    #[account(mut)]
+    pub mint: InterfaceAccount<'info, Mint>,
+    #[account(mut)]
+    pub recipent_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    pub sender_authority: Signer<'info>,
+    pub token_program: Interface<'info, TokenInterface>
+}
+
+
+#[derive(Accounts)]
+pub struct BurnSplToken<'info> {
+    #[account(mut)]
+    pub mint: InterfaceAccount<'info, Mint>,
+    #[account(mut)]
+    pub sender_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    pub sender_authority: Signer<'info>,
+    pub token_program: Interface<'info, TokenInterface>
+}
+
+
+#[account]
+pub struct TokenAuthority {
+    pub admin: Pubkey,
+    pub bump: u8,
+}
+
+#[account]
+pub struct TokenMetadata {
+    pub name: String,
+    pub symbol: String,
+    pub decimals: u8,
+    pub uri: Option<String>,
+    pub max_supply: Option<u64>,
+    pub admin: Pubkey,
+    pub mint: Pubkey,
+    pub bump: u8,
+}
+
+#[account]
+pub struct Escrow {
+    pub sender: Pubkey,
+    pub mint: Pubkey,
+    pub amount: u64,
+    pub seed: [u8; 32],
+    pub recipent: Option<Pubkey>,
+    pub claimed: bool,
+    pub bump:u8,
+
+}
